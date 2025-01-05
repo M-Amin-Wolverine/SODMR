d8208c75831d6d49eb77dd15c3081856a367706c
M-Amin-Wolverine <amin.khodadadi5246@gmail.com>
2025-01-05 17:12:42 +0330

    Update train_model.py

--- 3_train_model/train_model.py
+++ 3_train_model/train_model.py
@@ -1,35 +1,50 @@
-# train_model.py
-
 import numpy as np
 from alexnet import alexnet
 
+# Configuration
 training_dataset = "training_data_2021-02-16-1"
-
 WIDTH = 160
 HEIGHT = 120
 LR = 1e-3
 EPOCHS = 10
-MODEL_NAME = 'gta-{}-{}-{}-epochs-{}.model'.format(LR, 'alexnetv2',EPOCHS,training_dataset)        
+MODEL_NAME = f'gta-{LR}-alexnetv2-{EPOCHS}-epochs-{training_dataset}.model'
 
+# Initialize the model
 model = alexnet(WIDTH, HEIGHT, LR)
 
-for i in range(EPOCHS):
-    
-    train_data = np.load("training_data/"+training_dataset+"-balanced.npy",allow_pickle=True)
+for epoch in range(EPOCHS):
+    print(f"Epoch {epoch + 1}/{EPOCHS}")
 
+    # Load training data
+    train_data = np.load(f"training_data/{training_dataset}-balanced.npy", allow_pickle=True)
+
+    # Split data into training and testing sets
     train = train_data[:-100]
     test = train_data[-100:]
 
-    X = np.array([i[0] for i in train]).reshape(-1,WIDTH,HEIGHT,1)
-    Y = [i[1] for i in train]
+    # Prepare training data
+    X = np.array([data[0] for data in train]).reshape(-1, WIDTH, HEIGHT, 1)
+    Y = [data[1] for data in train]
 
-    test_x = np.array([i[0] for i in test]).reshape(-1,WIDTH,HEIGHT,1)
-    test_y = [i[1] for i in test]
+    # Prepare testing data
+    test_x = np.array([data[0] for data in test]).reshape(-1, WIDTH, HEIGHT, 1)
+    test_y = [data[1] for data in test]
 
-    model.fit({'input': X}, {'targets': Y}, n_epoch=1, validation_set=({'input': test_x}, {'targets': test_y}), 
-        snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+    # Train the model
+    model.fit(
+        {'input': X}, {'targets': Y},
+        n_epoch=1,
+        validation_set=({'input': test_x}, {'targets': test_y}),
+        snapshot_step=500,
+        show_metric=True,
+        run_id=MODEL_NAME
+    )
 
-    model.save("model/"+MODEL_NAME)
+    # Save the model
+    model.save(f"model/{MODEL_NAME}")
 
+print("Training complete.")
 
-# tensorboard --logdir=foo:D:\devel\gta\log
+# Command to run TensorBoard
+print("Run the following command to launch TensorBoard:")
+print("tensorboard --logdir=foo:D:\\devel\\gta\\log")
