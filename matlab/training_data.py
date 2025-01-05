d7eb2f10f673a831a853920eb183609a2bd42a22
M-Amin-Wolverine <amin.khodadadi5246@gmail.com>
2025-01-05 17:31:02 +0330

    Update training_data.py

--- matlab/training_data.py
+++ matlab/training_data.py
@@ -1,10 +1,23 @@
-# training_data.py
+import streamlit as st
 import numpy as np
+import os
+import cv2
 
-def load_data(npfile):
-    # npfile = "training_data.npy"
-    training_data = np.load(npfile,allow_pickle=True) 
-    img_array = []
-    for i in range(len(training_data)):
-        img_array.append(training_data[i][0])
-    return img_array
+# Cache the function to load images, suppress spinner display
+@st.cache(show_spinner=False)
+def load_image_data():
+    # Load the training data from the specified file
+    training_data = np.load("training_data/training_data-0.npy", allow_pickle=True)
+    return training_data
+
+# Sidebar title and slider
+st.sidebar.title("GTAV Dataset Viewer")
+images = load_image_data()
+frame_id = st.sidebar.slider("Select Frame", 0, len(images) - 1, 0)
+
+# Display the recorded time for the selected frame
+st.sidebar.markdown("## Recorded Time:")
+st.sidebar.write(images[frame_id][1])
+
+# Display the selected frame image
+st.image(images[frame_id][0], width=600, caption=f"Frame {frame_id}")
