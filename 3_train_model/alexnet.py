74aa7c8937ebf8fccb9b9e626c630d761f4c19df
M-Amin-Wolverine <amin.khodadadi5246@gmail.com>
2025-01-05 17:11:30 +0330

    Update alexnet.py

--- 3_train_model/alexnet.py
+++ 3_train_model/alexnet.py
@@ -1,10 +1,9 @@
-# alexnet.py
-
-""" AlexNet.
-Applying 'Alexnet' to Oxford's 17 Category Flower Dataset classification task.
+"""
+AlexNet.
+Applying 'AlexNet' to Oxford's 17 Category Flower Dataset classification task.
 References:
-    - Alex Krizhevsky, Ilya Sutskever and Geoffrey E. Hinton. ImageNet
-    Classification with Deep Convolutional Neural Networks. NIPS, 2012.
+    - Alex Krizhevsky, Ilya Sutskever, and Geoffrey E. Hinton. ImageNet
+      Classification with Deep Convolutional Neural Networks. NIPS, 2012.
 
 Links:
     - [AlexNet Paper](http://papers.nips.cc/paper/4824-imagenet-classification-with-deep-convolutional-neural-networks.pdf)
@@ -17,27 +16,41 @@ from tflearn.layers.estimator import regression
 from tflearn.layers.normalization import local_response_normalization
 
 def alexnet(width, height, lr):
+    # Define the input shape
     network = input_data(shape=[None, width, height, 1], name='input')
+
+    # First convolutional layer
     network = conv_2d(network, 96, 11, strides=4, activation='relu')
     network = max_pool_2d(network, 3, strides=2)
     network = local_response_normalization(network)
+
+    # Second convolutional layer
     network = conv_2d(network, 256, 5, activation='relu')
     network = max_pool_2d(network, 3, strides=2)
     network = local_response_normalization(network)
+
+    # Third, fourth, and fifth convolutional layers
     network = conv_2d(network, 384, 3, activation='relu')
     network = conv_2d(network, 384, 3, activation='relu')
     network = conv_2d(network, 256, 3, activation='relu')
     network = max_pool_2d(network, 3, strides=2)
     network = local_response_normalization(network)
+
+    # Fully connected layers
     network = fully_connected(network, 4096, activation='tanh')
     network = dropout(network, 0.5)
     network = fully_connected(network, 4096, activation='tanh')
     network = dropout(network, 0.5)
+
+    # Output layer with softmax activation
     network = fully_connected(network, 9, activation='softmax')
+
+    # Define the regression layer
     network = regression(network, optimizer='momentum',
                          loss='categorical_crossentropy',
                          learning_rate=lr, name='targets')
 
+    # Create the model
     model = tflearn.DNN(network, checkpoint_path='model_alexnet',
                         max_checkpoints=1, tensorboard_verbose=2, tensorboard_dir='log')
 
